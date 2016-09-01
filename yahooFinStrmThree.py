b9dd864114df31da19c196e7a0417c295b8498fd
Gus Segura <kyanyoga@yahoo.com>
2016-09-01 16:56:56 -0500

    Create yahooFinStrmThree.py

---
+++ yahooFinStrmThree.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+from yahoo_finance import Share
+import time
+import os
+import pymongo
+
+# stocks
+stocks = ["YHOO", "HPE", "AMZN", "AAPL", "GOOGL", "TWTR", "FB"]
+print stocks.__len__()
+
+# mongo conn
+# client = pymongo.MongoClient("bsmmsea-2-2.cloudapp.net", 57017)
+client = pymongo.MongoClient("localhost", 27017)
+db = client.finstream
+findata = db.findata
+
+# main loop - infinite loop
+while(True):
+    # timestamp
+    t = time.strftime('%Y-%m-%dT%H:%M:%S')
+    
+    # write to csv file
+    import csv
+        
+    # filename
+    outfile = '/tmp/datastream/' + 'BSM_FIN_STRM' + t
+    # outfile = 'BSMLABS_fin_csvstream.csv'
+    
+    # loop counter
+    x = 1
+        
+    with open( outfile, 'wa' ) as csvfile:
+        fieldnames = ['ts','symbol', 'price', 'open', 'days_high', 'days_low', 'volume']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        
+        # write file header
+        # writer.writeheader() -- don't need header for big data
+        
+        # main loop - cycle X times based on length of stock array
+        while (x <= (stocks.__len__() * 5)):
+            # stock loop
+            for stock in stocks:
+                
+                # print header 
+                print ("Stock: %12s  TimeStamp: %20s" % (stock, t))
+                
+                # sample: yahoo = Share('YHOO')
+                # print t, yahoo.get_open(), yahoo.get_price(), yahoo.get_days_high(), yahoo.get_days_low()
+                
+                # update timestamp
+                t = time.strftime('%Y-%m-%dT%H:%M:%S')
+                
+                # get yahoo finacne information
+                shareinfo = Share(stock)
+                
+                # print share information
+                print ("lc: %d price: %s open: %s dayhigh: %s daylow: %s volume: %s" 
+                       % (x,
+                          shareinfo.get_price(), 
+                          shareinfo.get_open(), 
+                          shareinfo.get_days_high(), 
+                          shareinfo.get_days_low(), 
+                          shareinfo.get_volume() 
+                        ))
+                
+                # line space
+                print
+                        
+                # write data to csv file
+                writer.writerow({'ts':t,
+                                 'symbol':stock,
+                                 'price':shareinfo.get_price(),
+                                 'open':shareinfo.get_open(),
+                                 'days_high':shareinfo.get_days_high(),
+                                 'days_low':shareinfo.get_days_low(),
+                                 'volume':shareinfo.get_volume()})
+               
+                # writer.writerow({'first_name': 'Baked', 'last_name': 'Beans'})
+                
+                # write mongodb data
+                findata.insert({'ts':t,
+                                'symbol':stock,
+                                'price':shareinfo.get_price(),
+                                'open':shareinfo.get_open(),
+                                'days_high':shareinfo.get_days_high(),
+                                'days_low':shareinfo.get_days_low(),
+                                'volume':shareinfo.get_volume()})
+                
+                # sleep
+                time.sleep(5.00)
+                
+                # increment counter
+                x += 1
+                    
+    # close streamed file
+    csvfile.close()
+    
+    #sleep 5 seconds - not much faster at .250
+    #time.sleep(15)
+    
+    
+    # find . -type f -name 'BSM*' -exec cat {} + >> stream.csv
